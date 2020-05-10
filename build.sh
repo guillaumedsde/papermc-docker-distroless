e4b5b14255772520ba80f2d5a0768581fd949863
guillaumedsde <guillaume@desusanne.com>
2020-05-10 19:03:44 +0200

    ci: add build script and gitlab ci

---
+++ build.sh
@@ -0,0 +1,27 @@
+# get a list of papermc versions
+versions=($(curl -k https://papermc.io/api/v1/paper/ | jq -r '.versions | @sh' -)) && printf '%s\n' "${arr[@]}"
+
+# build all versions
+for i in "${!versions[@]}"; do
+    # clean version number
+    version=$(echo "${versions[$i]//\'/}")
+
+    # image tag
+    current_image="${CI_REGISTRY_IMAGE}:${version}"
+
+    echo "Building ${current_image}"
+
+    continue
+
+    # build and push image
+    docker build . --pull --tag ${current_image} --build-arg PAPERMC_VERSION=${version}
+    docker push ${current_image}
+
+    # tag and push latest if latest version
+    if [ $i == 0 ]; then
+        latest_image=${CI_REGISTRY_IMAGE}:latest
+        echo "Building ${latest_image}"
+        docker tag ${current_image} ${latest_image}
+        docker push ${latest_image}
+    fi
+done
